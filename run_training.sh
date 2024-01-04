6606d56149850392e7d96a4186da50be7f01eba7
Yu Li <lyo.gavin@gmail.com>
2024-01-04 15:11:10 -0600

    v2

--- run_training.sh
+++ run_training.sh
@@ -15,4 +15,4 @@ OUTPUT_PATH=output_$run_id
 
 
 
- torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/v1/sprite_sheet_training.yaml --wandb  --output_dir $OUTPUT_PATH | tee -a training_log_$run_id.log
+ torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/v2/sprite_sheet_training.yaml --wandb  --output_dir $OUTPUT_PATH | tee -a training_log_$run_id.log
