5c8ba7efb6379f0a6e1407074abe518394010ad1
Yu Li <lyo.gavin@gmail.com>
2024-01-04 10:50:33 -0600

    run id

--- run_training.sh
+++ run_training.sh
@@ -1,1 +1,17 @@
-nohup torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/v1/sprite_sheet_training.yaml --wandb &
+
+
+
+set -x -e
+
+run_id=$(date +%s)
+echo "RUN ID: $run_ts"
+
+echo "START TIME: $(date)"
+
+echo 'log to'
+
+echo training_log_$run_id.log
+
+
+
+ torchrun --nnodes=1 --nproc_per_node=1 train.py --config configs/training/v1/sprite_sheet_training.yaml --wandb | tee -a training_log_$run_id.log
